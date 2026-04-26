272e43fa8702b083b7c9aa6dc874cfb1660342ea
Lucilia Luciano <lucilialuciano@gmail.com>
2026-04-26 11:31:11 -0300

    Adicionar arquivos de projeto.

---
+++ POC_Lucilia-backend-2026/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace POC_Lucilia_backend_2026.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
